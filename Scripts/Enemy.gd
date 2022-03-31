85205fe9540187055e4e5f839a93b46f3dffa56f
KubaJaky <93349535+KubaJaky@users.noreply.github.com>
2022-03-31 23:13:40 +0200

    Update Enemy.gd

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,8 +1,17 @@
 extends KinematicBody2D
 
 onready var player = get_tree().get_root().get_node("Node2D/YSort/Player")
+onready var tower = get_tree().get_root().get_node("Node2D/YSort/Tower")
 
 var target = Vector2()
 var velocity = Vector2()
 
-var speed :int = 100
+var can_move :bool = true
+var speed :int = 150
+
+
+func _physics_process(delta):
+	velocity = position.direction_to(target) * speed
+	target = tower.global_position
+	if can_move == true:
+		velocity = move_and_slide(velocity)
